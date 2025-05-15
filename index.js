3daa2c3de1b688799421b4e97dc0f91fa83ca32a
Tushar <153347682+Web-Tushar@users.noreply.github.com>
2025-05-15 23:08:53 +0600

    password generate

---
+++ index.js
@@ -0,0 +1,34 @@
+const passwordbox = document.querySelector("#password")
+const input_press = document.querySelector(".input_press")
+
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const number = "0123456789"
+const symbole = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+
+const allchars= upperCase+ lowerCase+number+symbole;
+const lent = 12;
+
+function crearPassword() {
+     let password ="";
+     password += upperCase[Math.floor(Math.random() * upperCase.length)]
+     password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+     password += number[Math.floor(Math.random() * number.length)]
+     password += symbole[Math.floor(Math.random() * symbole.length)]
+
+     while(lent>password.length){
+     password += allchars[Math.floor(Math.random() * allchars.length)]
+          
+
+     }
+
+     passwordbox.value=password;
+
+}
+
+
+input_press.addEventListener("click",function(){
+     crearPassword()
+})
